848dc52bec0ab55f11e49f10aa0c51f6e6341b59
cheng yiyang <qtcyy2005@gmail.com>
2025-08-07 08:55:18 +0800

    fix: 🐛 流返回

--- llm/llm_chat/llm_func.py
+++ llm/llm_chat/llm_func.py
@@ -1,7 +1,7 @@
 import os
-from typing import Any, AsyncGenerator
+from typing import Any, AsyncGenerator, Coroutine
 
-from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.messages import HumanMessage, AIMessage, BaseMessage
 from langchain_openai import ChatOpenAI
 
 from llm.llm_chat.chat_schema import AgentState
@@ -12,14 +12,15 @@ os.environ["OPENAI_API_BASE"] = "https://api.siliconflow.cn/v1"
 llm = ChatOpenAI(model="Qwen/Qwen2.5-7B-Instruct", verbose=True, temperature=0.8)
 
 
-async def chatbot(state: AgentState) -> AsyncGenerator[dict[str, list[AIMessage]], Any]:
+async def chatbot(state: AgentState) -> dict[str, BaseMessage]:
     messages = state["messages"]
     print(f"messages: {messages}")
     full_message = ""
-
-    async for chunk in llm.astream(messages):
-        full_message += chunk.content
-        yield {"messages": [chunk.content]}
-    print(f"state: {state}")
-    print(f"full_message: {full_message}")
+    return {"messages": await llm.ainvoke(messages)}
+    # async for chunk in llm.astream(messages):
+    #     full_message += chunk.content
+    #     yield {"messages": [chunk]}
+    # print(f"state: {state}")
+    # print(f"full_message: {full_message}")
     # yield {"messages": full_message}
+    # return {"messages": [full_message]}
